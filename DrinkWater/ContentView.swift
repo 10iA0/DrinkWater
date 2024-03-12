0da156278b5956f780c370d2e00a221b410082ca
XIAO Jihang <jihang.xiao@connect.polyu.hk>
2024-03-12 12:55:54 +0800

    Initial Commit

---
+++ DrinkWater/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DrinkWater
+//
+//  Created by 肖霁航 on 2024/3/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DrinkWater/DrinkWaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkWaterApp.swift
+//  DrinkWater
+//
+//  Created by 肖霁航 on 2024/3/12.
+//
+
+import SwiftUI
+
+@main
+struct DrinkWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
